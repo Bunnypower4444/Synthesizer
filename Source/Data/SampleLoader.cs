0aa831a5881c62fdfabd2758adad2ed7c318f1b2
Bunnypower <bunnypower2k20@gmail.com>
2024-09-17 22:38:09 -0700

    Read 16-bit samples

--- Source/Data/SampleLoader.cs
+++ Source/Data/SampleLoader.cs
@@ -37,7 +37,27 @@ public class StreamSampleLoader : ISampleLoader
     public BinaryReader Reader;
     public uint SampleChunkStart;
     public uint SampleChunkSize;
-    private readonly SortedDictionary<uint, (uint Size, byte[] Data)> cache = [];
+
+    public struct SampleCache
+    {
+        /// <summary>
+        /// The size of the data in PCM data points (16-bit)
+        /// THIS IS NOT THE SIZE OF THE BYTE DATA (8-bit)!
+        /// </summary>
+        public uint Size;
+        /// <summary>
+        /// The byte data of the sample.
+        /// Note that the length of this array is NOT the size of sample (which is in 16-bit data points)
+        /// </summary>
+        public byte[] Data;
+
+        public static implicit operator SampleCache((uint Size, byte[] Data) tuple)
+            => new() { Size = tuple.Size, Data = tuple.Data };
+    }
+    private readonly SortedDictionary<uint, SampleCache> cache = [];
+
+    private static int ByteIndex(uint s16Index)
+        => (int)(s16Index * 2);
 
     public byte[] GetSampleData(Sample sample)
     {
@@ -47,7 +67,7 @@ public class StreamSampleLoader : ISampleLoader
         if (SampleIsCached(sample, out var positionBeforeIndex))
         {
             var positionBefore = cache.Keys.ToArray()[positionBeforeIndex];
-            return cache[positionBefore].Data[(int)(sample.StartIndex - positionBefore)..(int)(sample.EndIndex - positionBefore)];
+            return cache[positionBefore].Data[ByteIndex(sample.StartIndex - positionBefore)..ByteIndex(sample.EndIndex - positionBefore)];
         }
         
         PreloadSample(sample, positionBeforeIndex);
@@ -58,7 +78,7 @@ public class StreamSampleLoader : ISampleLoader
         // Figure out the index of the element that would come BEFORE it (so minus one after inverting)
         if (index < 0)
             index = ~index - 1;
-        return cache[keys[index]].Data[(int)(sample.StartIndex - keys[index])..(int)(sample.EndIndex - keys[index])];
+        return cache[keys[index]].Data[ByteIndex(sample.StartIndex - keys[index])..ByteIndex(sample.EndIndex - keys[index])];
     }
 
     public bool SampleIsCached(Sample sample)
@@ -126,12 +146,13 @@ public class StreamSampleLoader : ISampleLoader
             loadEndIndex = positionAfter.Value;
 
         // Create the data array, and put existing data at the ends (if applicable)
+        // NOTE: these indices are 16-bit data indices, NOT byte data indices!
         uint arrayStartIndex = loadStartIndex != sample.StartIndex
             ? positionBefore!.Value : sample.StartIndex;
         uint arrayEndIndex = loadEndIndex != sample.EndIndex
             ? positionAfter!.Value + cache[positionAfter.Value].Size : sample.EndIndex;
         
-        byte[] data = new byte[arrayEndIndex - arrayStartIndex];
+        byte[] data = new byte[ByteIndex(arrayEndIndex - arrayStartIndex)];
 
         // The indices will be different if we combined
         if (loadStartIndex != sample.StartIndex)
@@ -142,15 +163,15 @@ public class StreamSampleLoader : ISampleLoader
 
         if (loadEndIndex != sample.EndIndex)
         {
-            cache[keys[positionBeforeIndex + 1]].Data.CopyTo(data, loadEndIndex - arrayStartIndex);
+            cache[keys[positionBeforeIndex + 1]].Data.CopyTo(data, ByteIndex(loadEndIndex - arrayStartIndex));
             cache.Remove(keys[positionBeforeIndex + 1]);
         }
 
         // Read the data
-        Reader.BaseStream.Position = SampleChunkStart + arrayStartIndex;
-        Reader.Read(data, (int)(loadStartIndex - arrayStartIndex), (int)(loadEndIndex - loadStartIndex));
+        Reader.BaseStream.Position = SampleChunkStart + ByteIndex(arrayStartIndex);
+        Reader.Read(data, ByteIndex(loadStartIndex - arrayStartIndex), ByteIndex(loadEndIndex - loadStartIndex));
 
-        cache[arrayStartIndex] = ((uint)data.Length, data);
+        cache[arrayStartIndex] = (arrayEndIndex - arrayStartIndex, data);
     }
 
     public void ClearCache()
@@ -169,10 +190,23 @@ public class StreamSampleLoader : ISampleLoader
 
     #if StreamSampleLoaderTests
     
+    private static byte[] ShortsToBytes(short[] shorts)
+    {
+        byte[] bytes = new byte[shorts.Length * 2];
+        Buffer.BlockCopy(shorts, 0, bytes, 0, shorts.Length * 2);
+        return bytes;
+    }
+
+    private static bool SequenceEqual(byte[] array, short[] shortArray)
+    {
+        return array.SequenceEqual(ShortsToBytes(shortArray));
+    }
+
     public static void Main()
     {
+
         // Use a MemoryStream to provide fake sample data
-        byte[] data = [
+        short[] data = [
             255, 255, 255, 255, 255, 255,
             0  , 1  , 2  , 3  , 4  , 5  , 6  , 7  , 8  , 9  ,
             10 , 11 , 12 , 13 , 14 , 15 , 16 , 17 , 18 , 19 ,
@@ -181,11 +215,11 @@ public class StreamSampleLoader : ISampleLoader
         ];
 
         using var stream = new MemoryStream(
-            data,
+            ShortsToBytes(data),
             false
         );
 
-        using var loader = new StreamSampleLoader(stream, 6, 30);
+        using var loader = new StreamSampleLoader(stream, (uint)ByteIndex(6), (uint)ByteIndex(30));
 
         static Sample SampleFrom(uint start, uint end)
             => new() { StartIndex = start, EndIndex = end };
@@ -195,28 +229,28 @@ public class StreamSampleLoader : ISampleLoader
         loader.PreloadSample(SampleFrom(10, 20));
         Debug.Assert(loader.cache.ContainsKey(10));
         Debug.Assert(loader.cache[10].Size == 10);
-        Debug.Assert(loader.cache[10].Data.SequenceEqual(new byte[] {10, 11, 12, 13, 14, 15, 16, 17, 18, 19}));
+        Debug.Assert(SequenceEqual(loader.cache[10].Data, [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]));
         Debug.Assert(true == loader.SampleIsCached(SampleFrom(10, 20)));
         Debug.Assert(true == loader.SampleIsCached(SampleFrom(15, 18)));
         Debug.Assert(false == loader.SampleIsCached(SampleFrom(8, 15)));
         Debug.Assert(false == loader.SampleIsCached(SampleFrom(12, 22)));
-        Debug.Assert(loader.GetSampleData(SampleFrom(15, 20)).SequenceEqual(new byte[] {15, 16, 17, 18, 19}));
+        Debug.Assert(SequenceEqual(loader.GetSampleData(SampleFrom(15, 20)), [15, 16, 17, 18, 19]));
         Debug.Assert(1 == loader.cache.Count);
 
-        Debug.Assert(loader.GetSampleData(SampleFrom(20, 25)).SequenceEqual(new byte[] {20, 21, 22, 23, 24}));
+        Debug.Assert(SequenceEqual(loader.GetSampleData(SampleFrom(20, 25)), [20, 21, 22, 23, 24]));
         Debug.Assert(1 == loader.cache.Count);
-        Debug.Assert(loader.cache[10].Data.SequenceEqual(new byte[] {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24}));
+        Debug.Assert(SequenceEqual(loader.cache[10].Data, [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]));
         
-        Debug.Assert(loader.GetSampleData(SampleFrom(2, 5)).SequenceEqual(new byte[] {2, 3, 4}));
+        Debug.Assert(SequenceEqual(loader.GetSampleData(SampleFrom(2, 5)), [2, 3, 4]));
         Debug.Assert(2 == loader.cache.Count);
         
-        Debug.Assert(loader.GetSampleData(SampleFrom(5, 12)).SequenceEqual(new byte[] {5, 6, 7, 8, 9, 10, 11}));
+        Debug.Assert(SequenceEqual(loader.GetSampleData(SampleFrom(5, 12)), [5, 6, 7, 8, 9, 10, 11]));
         Debug.Assert(1 == loader.cache.Count);
-        Debug.Assert(loader.cache[2].Data.SequenceEqual(new byte[] {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24}));
+        Debug.Assert(SequenceEqual(loader.cache[2].Data, [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]));
 
-        Debug.Assert(loader.GetSampleData(SampleFrom(0, 5)).SequenceEqual(new byte[] {0, 1, 2, 3, 4}));
+        Debug.Assert(SequenceEqual(loader.GetSampleData(SampleFrom(0, 5)), [0, 1, 2, 3, 4]));
         Debug.Assert(1 == loader.cache.Count);
-        Debug.Assert(loader.cache[0].Data.SequenceEqual(new byte[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24}));
+        Debug.Assert(SequenceEqual(loader.cache[0].Data, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]));
 
         Log.Info("All tests passed for StreamSampleLoader :)");
     }
